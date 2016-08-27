2aa1af6cacb4dd67065ec42ebf7747fa917c9f53
Hercules Karkow <heco_karkow98@hotmail.com>
2016-08-26 22:18:00 -0300

    atibutos adicionados

--- main.c
+++ main.c
@@ -24,10 +24,50 @@ int main(int argc, char *argv[])
 		for(k=0; k<4; k++)
 		{
 			// Insere um valor rondomico em uma posição da matriz
-			matriz[j][k] = rand()%50;
+			//matriz[j][k] = rand()%50;
+			
+			// Insere o indice de coluna na matriz
+			matriz[j][k] = k;
 			
 			// Imprime o valor da posição atual da matrix
 			printf("%02d ", matriz[j][k]);
+		}
+		
+		// Quebra a linha
+		printf("\n");
+	}
+	
+	// Imprime uma mensagem de identificação
+	printf("\n Os elementos da diagonal principal sao: \n");
+	
+	// Efetua a varredura da matriz
+	for(j=0; j<4; j++)
+	{
+		
+		// Imprime os elemntos da diagonal
+		printf("%02d ", matriz[j][j]);
+	}
+	
+	// Imprime uma mensagem de identificação
+	printf("\nOs elementos restantes da matriz sao: \n");
+	
+	// Laço que varre a linha 
+	for(j=0; j<4; j++)
+	{
+		
+		// Laço que varre a coluna
+		for(k=0; k<4; k++)
+		{	
+			// Se os indices forem iguais (diagonal principal)
+			if(j == k)
+			{
+				printf("-- ");
+			}
+			else
+			{
+				// Senão, imprime o valor da posição atual da matriz
+				printf("%02d ", matriz[j][k]);
+			}
 		}
 		
 		// Quebra a linha
